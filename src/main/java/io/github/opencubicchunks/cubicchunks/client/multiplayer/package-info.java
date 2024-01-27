d7bfe4fb1813dd1afd064b32dde45645ddbab94c
Tom Martin <tom.martin1239@gmail.com>
2024-01-27 00:45:54 +0000

    Fix generatePackageInfo

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/client/multiplayer/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package io.github.opencubicchunks.cubicchunks.client.multiplayer;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import io.github.opencubicchunks.cc_core.annotation.MethodsReturnNonnullByDefault;
