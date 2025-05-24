dcadb75e17f73e6963aabb3ceaf2eee43d974139
Seelder <zlschect@gmail.com>
2025-05-24 12:40:44 -0700

    Added a packet handling test

--- src/test/java/io/github/opencubicchunks/cubicchunks/test/network/TestCCClientboundLevelCubeWithLightPacket.java
+++ src/test/java/io/github/opencubicchunks/cubicchunks/test/network/TestCCClientboundLevelCubeWithLightPacket.java
@@ -2,23 +2,32 @@ package io.github.opencubicchunks.cubicchunks.test.network;
 
 import static io.github.opencubicchunks.cubicchunks.testutils.Misc.assertDeepEquals;
 import static io.github.opencubicchunks.cubicchunks.testutils.Misc.generateRandomLevelCube;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
 import java.util.Random;
 
 import io.github.opencubicchunks.cc_core.api.CubePos;
 import io.github.opencubicchunks.cubicchunks.CanBeCubic;
+import io.github.opencubicchunks.cubicchunks.client.multiplayer.ClientCubeCache;
 import io.github.opencubicchunks.cubicchunks.network.CCClientboundLevelCubeWithLightPacket;
 import io.github.opencubicchunks.cubicchunks.testutils.BaseTest;
 import io.github.opencubicchunks.cubicchunks.world.level.cube.LevelCube;
 import io.netty.buffer.Unpooled;
+import net.minecraft.client.multiplayer.ClientChunkCache;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.network.FriendlyByteBuf;
+import net.neoforged.neoforge.network.handling.PlayPayloadContext;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class TestCCClientboundLevelCubeWithLightPacket extends BaseTest {
+
     @Test
     public void serdeTest() {
         ClientLevel clientLevelMock = mock(Mockito.RETURNS_DEEP_STUBS);
@@ -41,5 +50,30 @@ public class TestCCClientboundLevelCubeWithLightPacket extends BaseTest {
 
         assertDeepEquals(new CCClientboundLevelCubeWithLightPacket(buf1), packet1);
         assertDeepEquals(new CCClientboundLevelCubeWithLightPacket(buf2), packet2);
+    }
+
+
+    @Test
+    public void handlerTest() {
+        PlayPayloadContext payloadContextMock = mock();
+        ClientLevel clientLevelMock = mock(Mockito.RETURNS_DEEP_STUBS);
+        ClientChunkCache clientChunkCacheMock = mock(ClientChunkCache.class);
+        when(clientLevelMock.getChunkSource()).thenReturn(clientChunkCacheMock);
+        when(((CanBeCubic) clientLevelMock).cc_isCubic()).thenReturn(true);
+        when(clientLevelMock.getHeight()).thenReturn(384);
+        when(clientLevelMock.getSectionsCount()).thenReturn(24);
+
+        when(payloadContextMock.level()).thenReturn(Optional.of(clientLevelMock));
+
+        var pos = CubePos.of(10, -2, 4);
+        var cube = generateRandomLevelCube(clientLevelMock, pos, new Random(3333));
+
+        var packet = new CCClientboundLevelCubeWithLightPacket(cube);
+
+        var handler = new CCClientboundLevelCubeWithLightPacket.Handler();
+
+        handler.handle(packet, payloadContextMock);
+
+        ((ClientCubeCache) verify(clientChunkCacheMock, times(1))).cc_replaceWithPacketData(eq(pos.getX()), eq(pos.getY()), eq(pos.getZ()), any(), any(), any());
     }
 }
