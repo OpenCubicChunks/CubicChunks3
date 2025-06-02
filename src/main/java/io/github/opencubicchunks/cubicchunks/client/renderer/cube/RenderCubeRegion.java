1929a1e01dcc4869da8be3b2fb93148bd6f74f8a
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2025-06-02 19:50:30 +1200

    update RenderCubeRegion (TODO update tests)

--- src/main/java/io/github/opencubicchunks/cubicchunks/client/renderer/cube/RenderCubeRegion.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/client/renderer/cube/RenderCubeRegion.java
@@ -11,42 +11,41 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 
 public class RenderCubeRegion extends RenderChunkRegion {
-    private final int centerX;
-    private final int centerY;
-    private final int centerZ;
-    protected final RenderCube[][][] cubes;
+    private final int minCubeX;
+    private final int minCubeY;
+    private final int minCubeZ;
+    protected final RenderCube[] cubes;
 
-    public RenderCubeRegion(Level level, int centerX, int centerY, int centerZ, RenderCube[][][] cubes, @Nullable net.neoforged.neoforge.client.model.data.ModelDataManager.Snapshot modelDataManager) {
+    public RenderCubeRegion(Level level, int minCubeX, int minCubeY, int minCubeZ, RenderCube[] cubes, @Nullable it.unimi.dsi.fastutil.longs.Long2ObjectFunction<net.neoforged.neoforge.model.data.ModelData> modelDataSnapshot) {
         super(level, 0, 0, null);
         // TODO set modelDataManager on parent - requires an accessor mixin since we can't AT the NF constructor or field
-        this.centerX = centerX;
-        this.centerY = centerY;
-        this.centerZ = centerZ;
+        this.minCubeX = minCubeX;
+        this.minCubeY = minCubeY;
+        this.minCubeZ = minCubeZ;
         this.cubes = cubes;
     }
 
     @Override
     public BlockState getBlockState(BlockPos pos) {
-        int x = Coords.blockToCube(pos.getX()) - this.centerX;
-        int y = Coords.blockToCube(pos.getY()) - this.centerY;
-        int z = Coords.blockToCube(pos.getZ()) - this.centerZ;
-        return this.cubes[x][y][z].getBlockState(pos);
+        return this.getCube(Coords.blockToCube(pos.getX()), Coords.blockToCube(pos.getY()), Coords.blockToCube(pos.getZ())).getBlockState(pos);
     }
 
     @Override
     public FluidState getFluidState(BlockPos pos) {
-        int x = Coords.blockToCube(pos.getX()) - this.centerX;
-        int y = Coords.blockToCube(pos.getY()) - this.centerY;
-        int z = Coords.blockToCube(pos.getZ()) - this.centerZ;
-        return this.cubes[x][y][z].getBlockState(pos).getFluidState();
+        return this.getCube(Coords.blockToCube(pos.getX()), Coords.blockToCube(pos.getY()), Coords.blockToCube(pos.getZ())).getBlockState(pos).getFluidState();
     }
 
     @Nullable
     @Override
     public BlockEntity getBlockEntity(BlockPos pos) {
-        int x = Coords.blockToCube(pos.getX()) - this.centerX;
-        int y = Coords.blockToCube(pos.getY()) - this.centerY;
-        int z = Coords.blockToCube(pos.getZ()) - this.centerZ;
-        return this.cubes[x][y][z].getBlockEntity(pos);
+        return this.getCube(Coords.blockToCube(pos.getX()), Coords.blockToCube(pos.getY()), Coords.blockToCube(pos.getZ())).getBlockEntity(pos);
+    }
+
+    private RenderCube getCube(int cubeX, int cubeY, int cubeZ) {
+        return this.cubes[index(this.minCubeX, this.minCubeY, this.minCubeZ, cubeX, cubeY, cubeZ)];
+    }
+
+    public static int index(int minCubeX, int minCubeY, int minCubeZ, int cubeX, int cubeY, int cubeZ) {
+        return cubeX - minCubeX + (cubeZ - minCubeZ) * 3 + (cubeY - minCubeY) * 9;
     }
 }
