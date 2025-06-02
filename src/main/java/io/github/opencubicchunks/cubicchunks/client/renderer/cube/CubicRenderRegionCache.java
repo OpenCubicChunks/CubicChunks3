08877a87b6622c2fa014c9876a01747c92517871
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2025-06-02 20:15:20 +1200

    update RenderRegionCache

--- src/main/java/io/github/opencubicchunks/cubicchunks/client/renderer/cube/CubicRenderRegionCache.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/client/renderer/cube/CubicRenderRegionCache.java
@@ -2,9 +2,9 @@ package io.github.opencubicchunks.cubicchunks.client.renderer.cube;
 
 import javax.annotation.Nullable;
 
-import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.world.level.Level;
 
 public interface CubicRenderRegionCache {
-    @Nullable RenderCubeRegion cc_createRegion(Level level, BlockPos start, BlockPos end, int padding, boolean nullForEmpty);
+    @Nullable RenderCubeRegion cc_createRegion(Level level, SectionPos sectionPos, boolean nullForEmpty);
 }
--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/client/renderer/chunk/MixinRenderRegionCache.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/client/renderer/chunk/MixinRenderRegionCache.java
@@ -3,7 +3,6 @@ package io.github.opencubicchunks.cubicchunks.mixin.core.common.client.renderer.
 import javax.annotation.Nullable;
 
 import io.github.opencubicchunks.cc_core.api.CubePos;
-import io.github.opencubicchunks.cc_core.api.CubicConstants;
 import io.github.opencubicchunks.cc_core.utils.Coords;
 import io.github.opencubicchunks.cubicchunks.client.renderer.cube.CubicRenderRegionCache;
 import io.github.opencubicchunks.cubicchunks.client.renderer.cube.RenderCube;
@@ -13,64 +12,53 @@ import io.github.opencubicchunks.cubicchunks.world.level.CubicLevel;
 import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.minecraft.client.renderer.chunk.RenderRegionCache;
-import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(RenderRegionCache.class)
-public class MixinRenderRegionCache implements CubicRenderRegionCache {
+public abstract class MixinRenderRegionCache implements CubicRenderRegionCache {
     @Shadow private final Long2ObjectMap<RenderRegionCacheCubeInfo> chunkInfoCache = new Long2ObjectOpenHashMap<>();
 
     // TODO can we possibly do this with DASM + mixin? probably not?
-    @Override @Nullable public RenderCubeRegion cc_createRegion(Level level, BlockPos start, BlockPos end, int padding, boolean nullForEmpty) {
-        var cubicLevel = ((CubicLevel) level);
-        int cubeStartX = Coords.blockToCube(start.getX() - padding);
-        int cubeStartY = Coords.blockToCube(start.getY() - padding);
-        int cubeStartZ = Coords.blockToCube(start.getZ() - padding);
-        int cubeEndX = Coords.blockToCube(end.getX() + padding);
-        int cubeEndY = Coords.blockToCube(end.getY() + padding);
-        int cubeEndZ = Coords.blockToCube(end.getZ() + padding);
-        RenderRegionCacheCubeInfo[][][] arenderregioncache$chunkinfo = new RenderRegionCacheCubeInfo[cubeEndX - cubeStartX + 1][cubeEndY - cubeStartY + 1][cubeEndZ - cubeStartZ + 1];
+    @Override @Nullable public RenderCubeRegion cc_createRegion(Level level, SectionPos sectionPos, boolean nullForEmpty) {
+        int centerCubeX = Coords.sectionToCube(sectionPos.getX());
+        int centerCubeY = Coords.sectionToCube(sectionPos.getY());
+        int centerCubeZ = Coords.sectionToCube(sectionPos.getZ());
+        var centerCubeInfo = this.cc_getChunkInfo(level, centerCubeX, centerCubeY, centerCubeZ);
+//        if (nullForEmpty && centerCubeInfo.cube().isSectionEmpty(sectionPos.y())) { // TODO need a proper isSectionEmpty on CubeAccess
+//            return null;
+//        }
+        int cubeStartX = centerCubeX - 1;
+        int cubeStartY = centerCubeY - 1;
+        int cubeStartZ = centerCubeZ - 1;
+        int cubeEndX = centerCubeX + 1;
+        int cubeEndY = centerCubeY + 1;
+        int cubeEndZ = centerCubeZ + 1;
+        RenderCube[] renderCubes = new RenderCube[27];
 
         for(int cubeX = cubeStartX; cubeX <= cubeEndX; ++cubeX) {
             for(int cubeY = cubeStartY; cubeY <= cubeEndY; ++cubeY) {
                 for(int cubeZ = cubeStartZ; cubeZ <= cubeEndZ; ++cubeZ) {
-                    arenderregioncache$chunkinfo[cubeX - cubeStartX][cubeY - cubeStartY][cubeZ - cubeStartZ] = this.chunkInfoCache
-                        .computeIfAbsent(
-                            CubePos.asLong(cubeX, cubeY, cubeZ),
-                            cubePosLong -> new RenderRegionCacheCubeInfo(cubicLevel.cc_getCube(CubePos.extractX(cubePosLong), CubePos.extractY(cubePosLong), CubePos.extractZ(cubePosLong)))
-                        );
+                    int cubeIndex = RenderCubeRegion.index(cubeStartX, cubeStartY, cubeStartZ, cubeX, cubeY, cubeZ);
+                    var cubeInfo = cubeX == centerCubeX && cubeY == centerCubeY && cubeZ == centerCubeZ
+                        ? centerCubeInfo
+                        : this.cc_getChunkInfo(level, cubeX, cubeY, cubeZ);
+                    renderCubes[cubeIndex] = cubeInfo.renderCube();
                 }
             }
         }
 
-        if (nullForEmpty && cc_isAllEmpty(start, end, cubeStartX, cubeStartY, cubeStartZ, arenderregioncache$chunkinfo)) {
-            return null;
-        } else {
-            RenderCube[][][] arenderchunk = new RenderCube[cubeEndX - cubeStartX + 1][cubeEndY - cubeStartY + 1][cubeEndZ - cubeStartZ + 1];
-
-            for(int x = cubeStartX; x <= cubeEndX; ++x) {
-                for(int y = cubeStartY; y <= cubeEndY; ++y) {
-                    for(int z = cubeStartZ; z <= cubeEndZ; ++z) {
-                        arenderchunk[x - cubeStartX][y - cubeStartY][z - cubeStartZ] = arenderregioncache$chunkinfo[x - cubeStartX][y - cubeStartY][z - cubeStartZ].renderCube();
-                    }
-                }
-            }
-
-            var maxSection = CubicConstants.DIAMETER_IN_SECTIONS - 1;
-
-            var modelDataManager = level.getModelDataManager().snapshotSectionRegion(
-                Coords.cubeToSection(cubeStartX, 0), Coords.cubeToSection(cubeStartY, 0), Coords.cubeToSection(cubeStartZ, 0),
-                Coords.cubeToSection(cubeEndX, maxSection), Coords.cubeToSection(cubeEndY, maxSection), Coords.cubeToSection(cubeEndZ, maxSection)
-            );
-            return new RenderCubeRegion(level, cubeStartX, cubeStartY, cubeStartZ, arenderchunk, modelDataManager);
-        }
+        var modelDataManager = level.getModelDataManager().snapshotSectionRegion(cubeStartX, cubeStartY, cubeStartZ, cubeEndX, cubeEndY, cubeEndZ);
+        return new RenderCubeRegion(level, cubeStartX, cubeStartY, cubeStartZ, renderCubes, modelDataManager);
     }
 
-    private static boolean cc_isAllEmpty(BlockPos start, BlockPos end, int startX, int startY, int startZ, RenderRegionCacheCubeInfo[][][] infos) {
-        return false;
-        // TODO this seems to only be used as an optimization to avoid rendering empty sections so we probably don't need to implement it immediately
-        //      instead of using vanilla logic, probably just want to individually check that every section in the bounds is empty
+    private RenderRegionCacheCubeInfo cc_getChunkInfo(Level level, int cubeX, int cubeY, int cubeZ) {
+        return this.chunkInfoCache
+            .computeIfAbsent(
+                CubePos.asLong(cubeX, cubeY, cubeZ),
+                cubePosLong -> new RenderRegionCacheCubeInfo(((CubicLevel) level).cc_getCube(CubePos.extractX(cubePosLong), CubePos.extractY(cubePosLong), CubePos.extractZ(cubePosLong)))
+            );
     }
 }
