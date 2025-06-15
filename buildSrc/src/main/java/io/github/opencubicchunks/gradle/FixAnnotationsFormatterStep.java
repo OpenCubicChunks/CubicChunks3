ae5ebdbfe2623a59d4ef5904ea5e50d67913f437
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2025-06-15 19:39:41 +1200

    configure spotless+checkstyle

---
+++ buildSrc/src/main/java/io/github/opencubicchunks/gradle/FixAnnotationsFormatterStep.java
@@ -0,0 +1,28 @@
+package io.github.opencubicchunks.gradle;
+
+import java.io.File;
+
+import com.diffplug.spotless.FormatterStep;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class FixAnnotationsFormatterStep implements FormatterStep {
+    @Override public String getName() {
+        return "cc_fix_annotations";
+    }
+
+    private static final String[] ANNOTATIONS = { "@Shadow", "@Final", "@Mutable", "@Public", "@Override", "@Nullable", "@NotNull", };
+
+    @Override public @Nullable String format(String s, @NotNull File file) {
+        var outputString = s;
+        for (var annotation : ANNOTATIONS) {
+            // Remove newlines after the listed annotations
+            outputString = outputString.replaceAll(annotation + "\\n\\h*", annotation + " ");
+        }
+        return outputString;
+    }
+
+    @Override public void close() {
+
+    }
+}
